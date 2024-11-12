a5485a9ce8b378fa67195232fbafb898bfc8e9cb
Rosario Alonso <79224753+margarita1973@users.noreply.github.com>
2024-11-12 21:55:49 +0000

    Starwars modeling

--- src/models.py
+++ src/models.py
@@ -1,29 +1,67 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, UniqueConstraint
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Users (Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(50), nullable=False, unique=True)
+    email = Column(String(50), nullable=False, unique=True)
+    password = Column(String(10), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    def to_dict(self):
+        return {}
+
+class Planets (Base):
+    __tablename__ = 'planets'
+    id = Column (primary_key=True, nullable=False) 
+    name = Column (String(30), unique=True, nullable=False)
+    url = Column(String(50), unique= True, nullable=False)
+    diameter= Column(Integer)
+    rotation_period =Column (Integer)
+    orbital_period = Column (Integer)
+    gravity = Column (String(20))
+    population = Column (Integer)
+    climate = Column (String(30))
+    terrain  = Column (String(30))
+    surface_water = Column (Integer)
+    created = Column (Date)
+    edited = Column(Date)
+
+    def to_dict(self):
+        return {}
+    
+    
+class People (Base):
+    __tablename__ = 'people'
+    id =Column(Integer, primary_key=True)
+    name = Column (String(30), unique=True, nullable=False)
+    url= Column (String(50), unique=True, nullable=False)     
+    height= Column (Integer)
+    mass = Column(Integer)
+    hair_color = Column (String(10))
+    skin_color = Column (String(10))
+    eye_color = Column (String(10))
+    birth_year = Column (Date)
+    gender = Column (String(10))
+    created = Column (Date)
+    edited = Column (Date)
+    homeworld = Column (String(50), ForeignKey(Planets.url))
+
+    def to_dict(self):
+        return {}
+
+class Favorites(Base): 
+    __tablename__= 'favorites'
+    id =Column (Integer, primary_key=True)
+    user_id = Column (Integer, ForeignKey (Users.id))
+    planet_name = Column(String(30), ForeignKey(Planets.name))
+    people_name = Column(String(30), ForeignKey(People.name))
 
     def to_dict(self):
         return {}
